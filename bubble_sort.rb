a30d49f562dacd904efce5a7eeb666d4c5280875
patrick <angelosp07@gmail.com>
2021-01-05 16:06:52 +0200

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(arr)
   (arr.length - 1).times do
-    arr.each_with_index do |num, j|
+    arr.each_with_index do |_num, j|
       if arr[j + 1] && arr[j] > arr[j + 1]
         temp = arr[j]
         arr[j] = arr[j + 1]
@@ -15,15 +15,15 @@ bubble_sort(test_arr)
 
 def bubble_sort_by(arr)
   (arr.length - 1).times do
-		arr.each_with_index do |num, j|
-			if arr[j + 1]
-				number = yield(arr[j], arr[j + 1])
-				if number.positive?
-					temp = arr[j]
-					arr[j] = arr[j + 1]
-					arr[j + 1] = temp
-				end
-			end
+    arr.each_with_index do |_num, j|
+      if arr[j + 1]
+        number = yield(arr[j], arr[j + 1])
+        if number.positive?
+          temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j + 1] = temp
+        end
+      end
     end
   end
 end
