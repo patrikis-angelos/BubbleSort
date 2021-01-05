2ab59faf6fff4f8c6623aa941150e41a5c56799d
patrick <angelosp07@gmail.com>
2021-01-05 16:49:46 +0200

    return array instead of int

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,35 +1,34 @@
 def bubble_sort(arr)
   (arr.length - 1).times do
     arr.each_with_index do |_num, j|
-      if arr[j + 1] && arr[j] > arr[j + 1]
+      next unless arr[j + 1] && arr[j] > arr[j + 1]
         temp = arr[j]
         arr[j] = arr[j + 1]
         arr[j + 1] = temp
-      end
     end
-  end
+	end
+  return arr
 end
 
 test_arr = [4, 3, 78, 2, 0, 2]
-bubble_sort(test_arr)
 
 def bubble_sort_by(arr)
   (arr.length - 1).times do
     arr.each_with_index do |_num, j|
       if arr[j + 1]
         number = yield(arr[j], arr[j + 1])
-        if number.positive?
+        next unless number.positive?
           temp = arr[j]
           arr[j] = arr[j + 1]
           arr[j + 1] = temp
-        end
       end
     end
-  end
+	end
+	return arr
 end
 
 bubble_sort_by(test_arr) do |cur, nex|
   cur - nex
 end
 
-p test_arr
+p bubble_sort(test_arr)
